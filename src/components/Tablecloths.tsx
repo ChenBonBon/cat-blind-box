4abf9c8033fc97cafa5b95dbc8c3bf9cf67952a6
BonBon <per_cherry@163.com>
2024-11-21 17:24:51 +0800

    新增幸运咪展示

--- src/components/Tablecloths.tsx
+++ src/components/Tablecloths.tsx
@@ -1,8 +1,17 @@
+import useApp from "../useApp";
+
 export default function Tablecloths() {
+  const { luckyColor } = useApp();
+
   return (
-    <img
-      src="/images/tablecloths.png"
-      className="absolute top-0 right-0 w-[28%]"
-    />
+    <div className="absolute top-0 right-0 w-[28%]">
+      {luckyColor && (
+        <img
+          src={`/images/cat-${luckyColor + 1}.png`}
+          className="absolute top-[15%] left-1/2 w-1/3"
+        />
+      )}
+      <img src="/images/tablecloths.png" className="w-full h-full" />
+    </div>
   );
 }
